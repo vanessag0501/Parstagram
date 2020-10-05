5b351185420d40f34add356a69d5f3609c9daa21
Vanessa Garcia <vanessag0501@gmail.com>
2020-10-05 16:53:28 -0400

    Setting up logging in and signing up, as well as installing parse pods and launching the camera

--- Instagram/AppDelegate.swift
+++ Instagram/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // --- Copy this only
+                
+                let parseConfig = ParseClientConfiguration {
+                        $0.applicationId = "C0SBlqcFRyasKavDTcqvwnAcSNZ8AxGSNG1DA3YN"
+                        $0.clientKey = "N9uOdI2NnhWdhJgl5wWoCNrWuYkD6Flxh1466dAQ"
+                        $0.server = "https://parseapi.back4app.com"
+                }
+                Parse.initialize(with: parseConfig)
+                
+                // --- end copy
         return true
     }
 
@@ -31,6 +43,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
+    
+
 
 
 }
