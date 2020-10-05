e96aec6b117be97765c016b3dc62e97d85b0d71b
Vanessa Garcia <vanessag0501@gmail.com>
2020-10-05 14:51:17 -0400

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Vanessa Garcia on 10/5/20.
+//  Copyright © 2020 Vanessa Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
